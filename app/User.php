bb7ca4374f94f6000c9ed72e5eac6c62b0f7c92c
broksainis <broksainis@gmail.com>
2018-01-18 19:59:20 +0200

    redirect inactive users

--- app/User.php
+++ app/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
     }
 
     public function isAdmin() {
-        if($this->role->name == 'administrators') {
+        if($this->role->name == 'administrators' && $this->active == 1) {
             return true;
         } else {
             return false;
