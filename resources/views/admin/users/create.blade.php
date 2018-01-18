6c66cdad2f955a5cf7b8fd39da4c93ee279694d2
broksainis <broksainis@gmail.com>
2018-01-18 14:04:28 +0200

    create user template

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
     <h1>Add User</h1>
+    {!! Form::open(['method' => 'POST', 'acion'=> 'AdminUsersController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Namee:') !!}
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+        </div>
 
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+        </div>
+
+    {!! Form::close() !!}
 @endsection
